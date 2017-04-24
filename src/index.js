7f26774f8e0a0b3275c760bc35618359c1c050bc
pl <pl@host.com>
2017-04-25 00:23:41 +0300

    refactoring App component to class component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
 import YTSearch from "youtube-api-search";
 
 const API_KEY = 'AIzaSyARgS2afrIcW6hMmksoH-iMqKjRK8dn0b4'; 
-YTSearch({
-	key: API_KEY,
-	term:'iphone6'}, function (data) {
-		console.log(data);
+
+
+class App extends Component {
+	
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term:'iphone6'}, (data) => {
+				this.setState({ videos: data })
+				console.log(this.state.videos);
+
+			}
+		); 
 	}
-);
 
-
-
-const App = () => {
-	return (
-		<div>
-			< SearchBar/>
-		</div>
-		)
-}
+	render(){
+		return (
+			<div>
+				< SearchBar/>
+			</div>
+		);
+	}
+}	
 
 ReactDOM.render(<App />, document.querySelector('.container')); 
